d4c5cb354ead2fa0113c6eb8e5127af3547e2c07
dinhductri202com <anous12bg@gmail.com>
2024-07-08 14:27:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+######Title Coming soon !
 ###unset
 unset mem
 unset core
@@ -9,13 +10,19 @@ yellow='\033[1;33m'
 blue='\033[1;34m'
 light_cyan='\033[1;96m'
 reset='\033[0m'
-#
+##set
+usr=`whoami`
+
+case $usr in
+   root) clear ;;
+   *) echo "Please Run as root!" ; exit ;;
+esac
 
 
 pan()
 {
 ### panner generator on website: https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
-echo -e """
+echo -e $yellow """
 ██╗   ██╗██╗  ██╗██╗         ██╗   ██╗██████╗ ███████╗
 ██║   ██║██║ ██╔╝██║         ██║   ██║██╔══██╗██╔════╝
 ██║   ██║█████╔╝ ██║         ██║   ██║██████╔╝███████╗
@@ -26,6 +33,7 @@ echo -e """
 }
 check()
 {
+echo -e $yellow "Check"
 mem=`free --mega|grep Mem:|awk '{ print $7}'` && printf $yellow"Your MemAvailable: "$reset && echo -e $green"$mem" MB $reset
 core=`lscpu|grep "CPU(s):" |awk '{print $2}'` && printf $yellow"Your Cores:"$reset && echo -e $green "$core" $reset
 }
@@ -44,3 +52,30 @@ esac
 unset kvm
 unset kvdiv
 }
+
+#
+
+setupd()
+{
+echo -e $blue" > Setup" $reset
+echo -e $yellow " ==> Update" $reset
+sudo apt update -y >/dev/null 2>&1
+echo -e $yellow " ==> Download Package(Ngrok,Qemu,7z)" $reset
+wget -qq https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -O ngrok.tgz
+tar -xzf ngrok.tgz -C /bin/
+sudo apt install p7zip-full p7zip-rar qemu-system -y >/dev/null 2>&1
+echo -e $green "Setup Done" $reset
+}
+ban
+printf "$blue paste your NGROK AUTHTOKEN:"reset""
+read -p " " ngrok
+ngrok authtoken $ngrok
+clear
+printf "$blue paste your ISO URL:"$reset""
+read -p " " isourl
+clear
+wget -qq $isourl -O os.iso  –show-progress
+check
+checkvm
+setupd
+qemu-img create -f qcow2 os.qcow2 60G
